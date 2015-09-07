0172cbf6c8d7c91c1bdc40d3f3e94b8cb9b4f171
Hasina <hasina.khatoon@mobiloittegroup.com>
2015-09-07 17:06:22 +0530

    letters

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,9 +6,9 @@ class PasswordResetsController < ApplicationController
    @user = User.find_by_email(params[:email])
     if @user
       @user.send_password_reset  
-      render :json => { :Response_code => 200,:Response_message => "Your password reset instrustions has been sent to your email."}  
+      render :json => { :response_code => 200,:response_message => "Your password reset instrustions has been sent to your email."}  
     else 
-      render :json => { :Response_code => 500,:Response_message =>"Email does not exists."}
+      render :json => { :response_code => 500,:response_message =>"Email does not exists."}
     end
   end
 
@@ -17,12 +17,12 @@ class PasswordResetsController < ApplicationController
 	@user = User.find_by_password_reset_token(params[:access_code])
   if @user
     if@user.password_reset_token_sent_at < 2.hours.ago
-      render :json => { :Response_code => 500,:Response_message => "Your access code has been expired"}	
+      render :json => { :response_code => 500,:response_message => "Your access code has been expired"}	
 	  else
-	   render :json => { :Response_code => 200,:Response_message => "Your can reset your password now "}	
+	   render :json => { :response_code => 200,:response_message => "Your can reset your password now "}	
     end
   else
- 	  render :json => { :Response_code => 500,:Response_message => "Please enter valid access code"}	
+ 	  render :json => { :response_code => 500,:response_message => "Please enter valid access code"}	
   end
  end
 
@@ -31,18 +31,18 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_id(params[:user_id])
     if @user
       if @user.update_attributes(:password=>params[:password],:password_confirmation=>params[:password_confirmation])
-    	   render :json => { :Response_code => 200,
-    	  	              :Response_message => "Password has been changed",
+    	   render :json => { :response_code => 200,
+    	  	              :response_message => "Password has been changed",
                         :user=> @user.as_json(:only=>[:name,:email,:DOB]),
     	  	              }	
       else
-         render :json => { :Response_code => 200,
-        	                :Response_message => "Somthing worng",
+         render :json => { :response_code => 200,
+        	                :response_message => "Somthing worng",
                           :user=>@user.errors.messages.map{ |k,v| "#{k.capitalize.to_s.gsub('_',' ')} #{v.first}"}.join(', ')+"."
         	               }	
       end  
     else
-        render :json => { :Response_code => 500,:Response_message => "User does not exists"}  
+        render :json => { :response_code => 500,:response_message => "User does not exists"}  
     end
   end
 end 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,28 +5,28 @@ class SessionsController < ApplicationController
       if user 
         if user.confirmation_token.nil? 
           if user.authenticate(params[:password])
-            render :json => { :Response_code => 200,:Response_message =>"You have successfully logged In"}
+            render :json => { :response_code => 200,:response_message =>"You have successfully logged In"}
           else
-            render :json => { :Response_code => 500,:Response_message =>"Email or password is invalid"}
+            render :json => { :response_code => 500,:response_message =>"Email or password is invalid"}
           end
         else
-          render :json => { :Response_code => 500,:Response_message =>"Your account is not verified yet, Please login to your registered email id and visit the link to verify your account."}
+          render :json => { :response_code => 500,:response_message =>"Your account is not verified yet, Please login to your registered email id and visit the link to verify your account."}
         end
       else
-            render :json => { :Response_code => 500,:Response_message =>"Email does not exist"}
+            render :json => { :response_code => 500,:response_message =>"Email does not exist"}
       end
    end
    
   def log_out
-      render :json =>  { :Response_code => 200,:Response_message =>"Successfully logged out."}
+      render :json =>  { :response_code => 200,:response_message =>"Successfully logged out."}
   end
 
   def social_auth
      @user = SocialAuthentication.user_authentication_from_socialmedia(params[:provider_name],params[:uid])
       if @user.present?
-       render :json => { :Response_code => 200,:Response_message =>"You have successfully logged In !"}
+       render :json => { :response_code => 200,:response_message =>"You have successfully logged In !"}
      else
-       render :json => { :Response_code => 500,:Response_message =>"Authentication failed"}
+       render :json => { :response_code => 500,:response_message =>"Authentication failed"}
     end
   end
 
